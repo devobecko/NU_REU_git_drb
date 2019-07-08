9d32dd21d1c928ec06a6a737c2f4f200a9c8fe9c
Devin Becker <devinbecker@Devins-MacBook-Air.local>
2019-07-08 13:54:58 -0500

    plot cosine instead of sine

--- python_plot.py
+++ python_plot.py
@@ -2,6 +2,6 @@ https://github.com/devobecko/NU_REU_git_drb.giimport matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 2*np.pi, 50)
-y = np.sin(x)
+y = np.cos(x)
 f, ax = plt.subplots(figsize=(8,5))
 ax.plot(x,y, color='darkgreen')
